80f4464728988b9df6694d636c0387cd80b4bd5a
Jose Pizarro <jpizarroberrios@gmail.com>
2026-05-13 11:24:38 -0400

    fix sintaxis

--- 01_creacion_base_datos.sql
+++ 01_creacion_base_datos.sql
@@ -11,7 +11,7 @@
 -- DROP DATABASE IF EXISTS proyecto_db;
 
 -- Crear base de datos principal
-CREATE DATABASE IF NOT EXISTS proyecto_db 
+CREATE DATABASE IF NOT EXISTS proyecto_db
 CHARACTER SET utf8mb4 
 COLLATE utf8mb4_unicode_ci;
 
